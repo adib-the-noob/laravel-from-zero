63a23a47490dcc9946552dad4b1dfe6086b9619e
adib-the-noob <mdadib550@gmail.com>
2025-01-02 15:29:37 +0600

    layout file added

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+</head>
+<body>
+    <nav>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <a href="/data">GET JSON</a>
+    </nav>
+
+    {{ $slot }}
+</body>
+</html>
